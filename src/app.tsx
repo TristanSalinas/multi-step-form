a655fa382b4152549eb0fc62e116ec5e2b06876d
Tristan Salinas <salinas.tristan@gmail.com>
2025-01-10 20:06:13 +0100

    refacto app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,6 @@ import { SubscriptionPage } from "./pages/subscription_page";
 import { LandingPage } from "./pages/landing_page";
 
 function App() {
-  //basename="/multi-step-form" => quick fix to match the {base:/multi-step-form/} in vite-config to deploy on github page
   return (
     <BrowserRouter basename="/multi-step-form">
       <Routes>
